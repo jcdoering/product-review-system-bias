694518e4ba40f78859ff7b478cf63297b4e78a42
Jeff Doering <jeffreycdoering@gmail.com>
2023-04-05 14:15:07 -0400

    First remote commits
    First steps in file framework

---
+++ run_sim.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 5 2023
+
+@author: jeffreydoering
+umdoerij@myumanitoba.ca
+
+This script is part of a project contributing to a PhD research stream at the 
+University of Manitoba by Jeffrey Doering
+"""
+
+import sim_setup.sim_iter
+
---
+++ sim_setup/sim_iter.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 5 2023
+
+@author: jeffreydoering
+umdoerij@myumanitoba.ca
+
+This script is part of a project contributing to a PhD research stream at the 
+University of Manitoba by Jeffrey Doering
+"""
+
+# Configuring a class to contain our iterative simulation code
